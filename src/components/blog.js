43add38b9cdfedfacbd206afa181ed426bafef3e
Dan Curilla <curilladaniel@gmail.com>
2021-12-26 14:02:31 -0700

    show details button complete

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import Toggleable from './toggle';
+
+const BlogDetails = ({ blog }) => {
+  return (
+    <ul className="blogdetails">
+      <li>
+        <strong>Author: </strong>
+        {blog.author}
+      </li>
+      {blog.url && (
+        <li>
+          <strong>URL: </strong>
+          {blog.url}
+        </li>
+      )}
+    </ul>
+  );
+};
 
 const Blog = ({ blog }) => {
   return (
     <div className="card">
       <h1>{blog.title}</h1>
-      <ul>
-        <li>
-          <strong>Author: </strong>
-          {blog.author}
-        </li>
-        {blog.url && (
-          <li>
-            <strong>URL: </strong>
-            {blog.url}
-          </li>
-        )}
-      </ul>
+      <Toggleable>
+        <BlogDetails blog={blog} />
+      </Toggleable>
     </div>
   );
 };
